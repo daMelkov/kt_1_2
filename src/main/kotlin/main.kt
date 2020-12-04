d6f8ebe0ebd8fb1ebf9a29c4b14ab8b138225eb2
Melkov Denis <d.a.melkov@gmail.com>
2020-12-05 00:12:22 +0300

    check exit

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -5,9 +5,14 @@ fun main() {
     val percentComission = .0075
 
     while(true) {
-        print("Введите сумму перевода в копейках:")
-        val amount = readLine()!!.toInt()
+        print("Введите сумму перевода в копейках (пустая строка - выход):")
+        val input = readLine()
+        if(input!!.isEmpty()) {
+            println("Работа завершена.")
+            return
+        }
 
+        val amount = readLine()!!.toInt()
         if(amount < minComission) {
             println("Сумма недостаточна для перевода. Минимум: $minComission")
             continue
@@ -15,10 +20,5 @@ fun main() {
 
         val comission = if(amount * percentComission > minComission) amount * percentComission else minComission
         println("Комиссия составит: $comission")
-
-        print("Продолжить работу, y/n?:")
-        if(readLine() == "n") {
-            return
-        }
     }
 }
