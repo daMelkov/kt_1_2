1b42586ea448811f1d0153c5fa472ff6e477e556
Melkov Denis <d.a.melkov@gmail.com>
2020-12-04 23:16:15 +0300

    initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,24 @@
+// Cyrillic in console: Run/Edit Configuration/VM options: -Dfile.encoding=UTF-8
+
+fun main() {
+    val minComission = 3500
+    val percentComission = .0075
+
+    while(true) {
+        print("Введите сумму перевода в копейках:")
+        val amount = readLine()!!.toInt()
+
+        if(amount < minComission) {
+            println("Сумма недостаточна для перевода. Минимум: $minComission")
+            continue
+        }
+
+        val comission = if(amount * percentComission > minComission) amount * percentComission else minComission
+        println("Комиссия составит: $comission")
+
+        print("Завершить работу, y/n?:")
+        if(readLine() == "y") {
+            return
+        }
+    }
+}
